8b5d01be199c0b13833cdaf95d1eb83b3622166c
ArthurAD24 <164798288+ArthurAD24@users.noreply.github.com>
2025-09-17 19:30:17 -0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fecip_2025
+{
+    internal class Class1
+    {
+        public static string[] nome = new string[500];
+        public static int[] cpf = new int[500];
+        public static string[] senha = new string[500];
+        public static double[] entrada = new double[500];
+        public static double[] saida = new double[500];
+        public static string[] meta_tipo = new string[500];
+        public static double[] meta_valor = new double[500];
+        public static double[] saldo = new double[500];
+    }
+}
---
+++ Receitas.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace Fecip_2025
+{
+    partial class Receitas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Receitas));
+            pictureBox1 = new PictureBox();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.Receitas;
+            pictureBox1.Location = new Point(0, -3);
+            pictureBox1.Margin = new Padding(3, 4, 3, 4);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(1451, 909);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.AppWorkspace;
+            button1.BackgroundImage = (Image)resources.GetObject("button1.BackgroundImage");
+            button1.Location = new Point(508, 299);
+            button1.Margin = new Padding(3, 4, 3, 4);
+            button1.Name = "button1";
+            button1.Size = new Size(450, 68);
+            button1.TabIndex = 1;
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // Receitas
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1445, 908);
+            Controls.Add(button1);
+            Controls.Add(pictureBox1);
+            Margin = new Padding(3, 4, 3, 4);
+            Name = "Receitas";
+            Text = "Receitas";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Button button1;
+    }
+}
